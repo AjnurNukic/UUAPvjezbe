47994162740c590fffee588b698e7af22ec09d70
Ajnur Nukic <ajnur.nukic2002@gmail.com>
2026-05-08 17:21:47 +0200

    vjezba 11

---
+++ vjezbe11/primjer3.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+
+sns.set_theme(style="whitegrid")
+df = sns.load_dataset("penguins")
+
+sns.histplot(df["flipper_length_mm"], bins=20, kde=True)
+plt.show()
+
+sns.histplot(df, x="flipper_length_mm", hue="species", kde=True)
+plt.show()
+
+fig, axes = plt.subplots(2,2,figsize=(10,6))
+mjere = ["bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"]
+
+for ax, col in zip(axes.flat, mjere):
+    sns.violinplot(data=df, x="species", y=col, ax=ax)
+    ax.set_title(col.replace("_", " "))
+
+    plt.suptitle("Mjere pingvina po vrsti")
+    plt.tight_layout()
+    plt.show()
---
+++ vjezbe11/primjer4.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+
+sns.set_theme(style="whitegrid")
+df = sns.load_dataset("penguins")
+
+r, p = stats.pearsonr(df["flipper_length_mm"],df["body_mass_g"])
+print(f"r = {r:.3f}, p = {p:.4f}")
+
+fig, ax = plt.subplots(figsize=(7,5))
+sns.regplot(data=df,x="flipper_length_mm",y="body_mass_g", hue="species",ax=ax)
+
+corr=df.corr(numeric_only=True)
+sns.heatmap(corr,annot=True,cmap="coolwarm",vmin=-1,vmax=1)
+
+sns.pairplot(df,hue="species", diag_kind="kde")
